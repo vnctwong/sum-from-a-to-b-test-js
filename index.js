1cda7b24b5965978277d96479f7c2605846fd015
vnctwong <vincentkywong@gmail.com>
2019-03-11 12:17:46 +0000

    uses recursive

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  // base case = fromN
+  if (fromN === toN) {
+    return fromN;
+    // increment until toN
+  } else {
+    return fromN + sum(fromN + 1, toN)
+  }
 }
 
 module.exports = sum;
